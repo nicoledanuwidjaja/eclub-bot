704cd27abb891346630d46c9d6c67426b1a41d6b
Nicole Danuwidjaja <nicole-danuwidjaja@zwift.com>
2020-11-18 02:09:54 -0500

    handle errors

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 from time import sleep
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
 
 dotenv_path = join(dirname(__file__), '.env')
@@ -13,7 +14,7 @@ USERNAME = os.environ.get("USERNAME")
 PASSWORD = os.environ.get("PASSWORD")
 
 # store follower username and name
-followers = {'rachna.lewis' : 'Rachna Lewis', 'krisp_thins' : 'Kristin', 'stegosaurusuuuuu' : 'Vera', 'liindazeng' : ''}
+followers = {}
 
 class FeedPage:
     def __init__(self, browser):
@@ -28,13 +29,23 @@ class FeedPage:
         follower_count = int(element.text.split()[0].replace(',', ''))
 
         for follower in range(1, follower_count):
-            profile_container = browser.find_element_by_xpath(
+            first_profile_container = browser.find_element_by_xpath(
                 '/html/body/div[5]/div/div/div[2]/ul/div/li[%s]/div/div/div' % follower)
             profile_name = browser.find_element_by_xpath(
                 '/html/body/div[5]/div/div/div[2]/ul/div/li[%s]/div/div/div[2]/div[1]' % follower).text
-            browser.execute_script("arguments[0].scrollIntoView();", profile_container)
-            username = browser.current_url
-            sleep(1)
+            browser.execute_script("arguments[0].scrollIntoView({behavior: 'smooth'});", first_profile_container)
+            browser.execute_script("arguments[0].scrollIntoView({behavior: 'smooth'});", first_profile_container)
+            # visit page to get url
+            first_profile_container.click()
+            username = browser.current_url[26:-1]
+            sleep(0.5)
+            self.browser.get('https://www.instagram.com/neu_eclub')
+            browser.find_element_by_xpath('//a[contains(@href, "%s")]' % "followers").click()
+            browser.execute_script("arguments[0].scrollIntoView({behavior: 'smooth'});", first_profile_container)
+            second_profile_container = browser.find_element_by_xpath(
+                '/html/body/div[5]/div/div/div[2]/ul/div/li[%s]/div/div/div' % follower)
+            sleep(0.5)
+            browser.execute_script("arguments[0].scrollIntoView({behavior: 'smooth'});", second_profile_container)
             followers[username] = profile_name
             print(followers)
 
@@ -48,26 +59,28 @@ class FeedPage:
 
         for username, name in followers.items():
             browser.get('https://www.instagram.com/direct/new/')
-            sleep(2)
+            sleep(3)
             first_name = name.split()[0] if name else 'there'
-            template = "Hey {name}! I hope you're having a wonderful week.\n" \
-                      "I just wanted to let you know that Demo Day - Northeastern's largest celebration of student " \
+            template = "Hey {name}! Just wanted to let you know that Demo Day - Northeastern's largest celebration of student " \
                       "entrepreneurship and innovation - is this Wednesday, November 18th, from 6-8:15pm EST. We would LOVE to see you there!\n" \
-                      "You'll meet the Husky Startup Challenge's top 12 startups compete for cash prizes in front of " \
+                      "You'll see the Husky Startup Challenge's top 12 startups compete for cash prizes in front of " \
                       "hundreds of people.\n" \
                       " You can sign up here:\n" \
-                      " https://tinyurl.com/demodayfall2020\n" \
-                      " Feel free to let us know if you have any questions!\n".format(name=first_name)
+                      " https://tinyurl.com/demodayfall2020\n".format(name=first_name)
             print(first_name)
             recipient = self.browser.find_element_by_css_selector("input[name='queryBox']")
             recipient.send_keys(username)
             browser.find_element_by_xpath('/html/body/div[2]/div/div/div[2]/div[2]/div/div/div[3]/button').click()
             browser.find_element_by_xpath('/html/body/div[2]/div/div/div/div/div[2]').click()
-            sleep(3)
+            sleep(1)
             # input message
-            message_input = self.browser.find_element_by_css_selector("textarea")
-            message_input.send_keys(template)
-            sleep(5)
+            try:
+                message_input = self.browser.find_element_by_css_selector("textarea")
+                message_input.send_keys(template)
+                sleep(4)
+            except NoSuchElementException:
+                print('Instagram limit reached!')
+                exit(1)
 
 
 class LoginPage:
@@ -98,7 +111,7 @@ def init(browser):
     login_page = home_page.go_to_login_page()
     login_page.login(USERNAME, PASSWORD)
     feed_page = FeedPage(browser)
-    # feed_page.collect_followers()
+    feed_page.collect_followers()
     feed_page.send_messages()
 
     errors = browser.find_elements_by_css_selector('#error_message')
@@ -109,4 +122,4 @@ def init(browser):
 browser = webdriver.Chrome(ChromeDriverManager().install())
 browser.implicitly_wait(5)
 init(browser)
-# browser.close()
+browser.close()
